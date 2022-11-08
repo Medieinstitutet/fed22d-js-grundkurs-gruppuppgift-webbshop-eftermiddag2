8dd13e5b576fe3457791ebdb6790ee6f83f67efa
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2022-11-08 18:56:16 +0100

    Added shopping cart indicator for amount of donuts

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const donutAmount = document.querySelectorAll('.donut-count');
 const donutIncrease = document.querySelectorAll('.donut-amount-increase');
 const donutDecrease = document.querySelectorAll('.donut-amount-reduce');
+const donutCountCart = document.getElementById('donut-counter-cart');
 var donutCount = [];
-
-
 //Puts all amounts of donuts to 0, and makes sure we have variables for every donut
 donutAmount.forEach((element, index) => { //Loops through every class with ".donut-count"
     donutCount.push(0); //Adds a count to an array, defaults to 0
@@ -15,6 +14,10 @@ donutIncrease.forEach((button, index) =>{//Loops over all increase buttons
     button.addEventListener('click', () => {//Adds eventlistener for on click
         donutCount[index]++; //Adds +1 amount
         donutAmount[index].innerHTML = donutCount[index] + " st"; //Updates text
+        donutCountCart.innerHTML = getDonutCount(); // Updates shopping cart count
+        if (getDonutCount() > 0){ //If previously was hidden, and is now supposed to be visible, show
+            donutCountCart.style.visibility = "visible";
+        }
         console.log("Increased donut #" + index + " to " + donutCount[index]);
     });
 });
@@ -23,9 +26,20 @@ donutDecrease.forEach((button, index) =>{//Loops over all decrease buttons
         if(donutCount[index] > 0){//Checks so it isn't already 0
             donutCount[index]--; //Adds -1 amount
             donutAmount[index].innerHTML = donutCount[index] + " st"; //Updates text
+            donutCountCart.innerHTML = getDonutCount(); //Updates shopping cart amount
+            if (getDonutCount() <= 0){ //If 0, hide
+                donutCountCart.style.visibility = "hidden";
+            }
             console.log("Decreased donut #" + index + " to " + donutCount[index]);
         } else {
             console.log("can't reduce below 0!");
         }
     });
 });
+
+
+//Gets the total amount of donuts
+function getDonutCount(){
+    var sum = donutCount.reduce((total, a) => total + a, 0);
+    return sum;
+}
