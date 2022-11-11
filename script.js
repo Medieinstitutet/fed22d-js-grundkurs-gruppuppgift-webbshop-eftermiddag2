a8fe93e60a6383d8125ce9f844449fb4b6f8ba7e
NANI <nani7dam@gmail.com>
2022-11-11 01:09:00 +0100

    :wrench: clean count and add sort func js. chance bit html

--- script.js
+++ script.js
@@ -1,77 +1,98 @@
-const donutAmount = document.querySelectorAll(".donut-count")
-const donutIncrease = document.querySelectorAll(".donut-amount-increase")
-const donutDecrease = document.querySelectorAll(".donut-amount-reduce")
-const donutCountCart = document.getElementById("donut-counter-cart")
-var donutCount = []
-let donuts = []
+const donutAmount = document.querySelectorAll(".donut-count");
+const donutIncrease = document.querySelectorAll(".donut-amount-increase");
+const donutDecrease = document.querySelectorAll(".donut-amount-reduce");
+const donutCountCart = document.getElementById("donut-counter-cart");
+const sortingButtons = document.querySelectorAll(".sorting-type");
 
+var donutCount = [];
+let donuts = [];
 //Puts all amounts of donuts to 0, and makes sure we have variables for every donut
-donutAmount.forEach((element, index) => {
+donutAmount.forEach((_, index) => {
   //Loops through every class with ".donut-count"
-  donutCount.push(0) //Adds a count to an array, defaults to 0
-  element.innerHTML = donutCount[index] + " st" //Sets the text to show it is 0 amount
-
-  //Creates an object to store all info name, price and count
   donuts.push({
+    //push objects to 'donuts'-array. Object with properties 'name, price, count'
     name: document.getElementsByClassName("donut-name")[index].innerHTML,
-    price: document
-      .getElementsByClassName("donut-price")
-      [index].innerHTML.replace("kr/st", ""),
+    price: document.getElementsByClassName("donut-price")[index].innerHTML,
     count: 0,
   })
 })
 
-//Button functionality
-donutIncrease.forEach((button, index) => {
-  //Loops over all increase buttons
-  button.addEventListener("click", () => {
-    //Adds eventlistener for on click
-    donutCount[index]++ //Adds +1 amount
-    donutAmount[index].innerHTML = donutCount[index] + " st" //Updates text
+//       donutCountCart.style.visibility = "visible"
 
-    //changing counts in object donuts
-    donuts[index].count = document
-      .getElementsByClassName("donut-count")
-      [index].innerHTML.replace(" st", "")
-    donutCountCart.innerHTML = getDonutCount().toString() + " kr" // Updates shopping cart count
-    if (getDonutCount() > 0) {
-      //If previously was hidden, and is now supposed to be visible, show
-      donutCountCart.style.visibility = "visible"
-    }
-    console.log("Increased donut #" + index + " to " + donutCount[index])
-  })
+// Callback function used in '.reduce'
+const cartSum = (acc, donut) => {
+  return acc + donut.price * donut.count
+}
+
+//Gets the total amount of donuts (cartSum)
+const getDonutCount = () => {
+  return donuts.reduce(cartSum, 0);
+}
+
+donutIncrease.forEach((button, index) => {  //Loops over all increase buttons
+const clickPlus = () => {
+  //Adds eventlistener to each button
+  donuts[index].count++; //Adds +1 amount
+  donutAmount[index].innerHTML = donuts[index].count; //Updates count text
+  donutCountCart.innerHTML = getDonutCount(); //Updates cart text
+}
+  button.addEventListener("click", clickPlus);
 })
 
 donutDecrease.forEach((button, index) => {
-  //Loops over all decrease buttons
-  button.addEventListener("click", () => {
-    //Adds eventlistener for on click
-    if (donutCount[index] > 0) {
-      //Checks so it isn't already 0
-      donutCount[index]-- //Adds -1 amount
-      donutAmount[index].innerHTML = donutCount[index] + " st" //Updates text
-
-      //changing counts in object donuts
-      donuts[index].count = document
-        .getElementsByClassName("donut-count")
-        [index].innerHTML.replace(" st", "")
-
-      donutCountCart.innerHTML = getDonutCount().toString() + " kr" //Updates shopping cart amount
-      if (getDonutCount() <= 0) {
-        //If 0, hide
-        donutCountCart.style.visibility = "hidden"
-      }
-      console.log("Decreased donut #" + index + " to " + donutCount[index])
-    } else {
-      console.log("can't reduce below 0!")
+  const clickMinus = () => {
+    if (donuts[index].count > 0) { // Only runs when count text is bigger than 0
+      donuts[index].count--; //Decrease -1 amount
+      donutAmount[index].innerHTML = donuts[index].count;
+      donutCountCart.innerHTML = getDonutCount();
     }
-  })
+  }
+  button.addEventListener("click", clickMinus);
 })
 
-//Gets the total amount of donuts
-function getDonutCount() {
-  let sum = donuts.reduce((acc, donut) => {
-    return acc + donut.price * donut.count
-  }, 0)
-  return sum
+const donutsContainer = document.getElementsByClassName("flex-content"); //array med div:en med ALLA munkar
+const mainDonuts = document.getElementById("sort-container"); //creates container for '.appendChild()'
+const form = document.getElementById("form-container"); // use for preference line at <form> in html
+
+const sortPriceButtonAsc = sortingButtons[0]; //Button Sort price: High to low.
+const sortPriceButtonDsc = sortingButtons[1]; // Button sort price: Low to high.
+
+const sortPrice = (index) => {
+  //creates a function for sorting price
+  let sortedArray = []; //  empty array for sorted preferences
+  let unSortedArray = []; //  empty array for comparison
+  donuts.forEach((donut, j) => {
+    // loop for every donut in 'donutsContainer'
+    sortedArray.push(donut.price); //extracts prices from donuts into 'sortedArray'
+    unSortedArray.push(   // extrac donuts prices from 'index.html' into 'unSortedArray'
+      document.getElementsByClassName("donut-price")[j].innerHTML //gets the price
+    );
+  })
+
+  const comparePrice = (price) => {  // compares all prices from unSortedArray with a price in sortedArray. until a match e.g 2===2
+    return price === sortedArray[index];
+  }
+
+  let foundIndex = unSortedArray.findIndex(comparePrice); //finds index in unSortedArray where elements matches with the sortedArray. It compares with the current index recieved in this function.
+  mainDonuts.insertBefore(donutsContainer[foundIndex], form); //insert the found donut above the reference; <form id="form-contianer"> in 'index.html'
+
+  if (index < sortedArray.length - 1) { //index has to be -1 (less) than length because start on index 1
+    sortPrice(index + 1); // if yes; then re-run function with +1 index. (starts on 2 next time etc)
+  }
 }
+
+const sortPriceAsc = () => { 
+  donuts.sort((donut1, donut2) => {   //sorts 'donuts array' by ascending price.
+    return donut2.price - donut1.price;
+  })
+  sortPrice(1) //sorts index.html when compared to 'donuts array'. Starts at 1, wants to compare 2nd index in sortedArray first. "bubble sort-theory"
+}
+sortPriceButtonAsc.addEventListener("click", sortPriceAsc); //onlick function for sort triggers
+
+const sortPriceDsc = () => { 
+  donuts.sort((donut1, donut2) => {
+    return donut1.price - donut2.price // sorts 'donuts array' by descending price
+  })
+  sortPrice(1);
+}
+sortPriceButtonDsc.addEventListener("click", sortPriceDsc); //onlick, run 'donuts.sort...'
